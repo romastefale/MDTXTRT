d5a95ff75571f1e3ae0d28af622e6b05a3c5c8bc
romastefale <romastefale@gmail.com>
2026-09-03 20:45:20 -0300

    feat: registrar rota de compartilhamento nativo

--- app.py
+++ app.py
@@ -5,5 +5,12 @@ import runtime_v2
 
 runtime_v2.install(main)
 
+
+def build_web_app() -> web.Application:
+    app = main.build_web_app()
+    app.router.add_post("/api/share-telegraph", runtime_v2.api_share_telegraph)
+    return app
+
+
 if __name__ == "__main__":
-    web.run_app(main.build_web_app(), host="0.0.0.0", port=main.PORT)
+    web.run_app(build_web_app(), host="0.0.0.0", port=main.PORT)
